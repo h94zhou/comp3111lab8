7bee193e4430b92f0973b0b497ce1ef0208d5866
Haiyi Zhou <h94zhou@edu.uwaterloo.ca>
2019-11-04 16:58:30 +0800

    redsox src code change

--- src/main/java/comp3111lab8/Library.java
+++ src/main/java/comp3111lab8/Library.java
@@ -5,7 +5,8 @@ package comp3111lab8;
 
 public class Library {
     public static void main(String arg[]) {
-	System.out.println("Go Yankees");
+    	System.out.println("Go Yankees");
+    	System.out.println("Actually go Redsox");
     }
     public boolean someLibraryMethod() {
         return true;
